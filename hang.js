a1e4f7bcb64eb64e6c2901f9e2db9833461a7a98
Maria <leoignis848@gmail.com>
2019-11-06 13:41:37 +0200

    first commit

---
+++ hang.js
@@ -0,0 +1,41 @@
+//создание массива со словами для игры
+var words = [
+    "program",
+    "people",
+    "apple",
+    "deck",
+    "witness"
+];
+// выбор случайного слова из массива
+var randomWord = words[Math.floor(Math.random() * words.length)];
+//создаем игровой массив 
+var answerArray = [];
+for (var i = 0; i < words.length; i++){
+    answerArray[i] = "_";
+    }
+var remainingLetters = randomWord.length;
+//игровой цикл
+while (remainingLetters > 0) {
+    //показываем состояние игры
+    alert(answerArray.join(" "));
+    //записываем вариант ответа
+    var guess = prompt("Угадайте букву или нажмите Отмена для выхода из игры.");
+    if (guess === null) {
+        //выходим из игрового цикла
+        break;
+    } else if (guess.length !== 1) {
+        alert("Пожалуйста,введите одну букву.");
+    } else {
+        //обновляем состояние игры
+        for (var j = 0; j < randomWord.length; j++) {
+            if (randomWord[j] === guess) {
+                answerArray[j] = guess;
+                remainingLetters--;
+            }
+        }
+    }
+//конец игрового цикла
+}
+//отображаем ответ и приветствуем игрока
+alert(answerArray.join(" "));
+alert("Отлично! Было загадано слово " + randomWord);
